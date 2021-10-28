e454e4b3be4979ed968b4399056908cc5cc3b19c
HibaEF <hiba.elwafi@esprit.tn>
2021-10-28 20:36:51 +0100

    changes

--- src/main/java/tn/esprit/spring/repository/RayonRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import tn.esprit.spring.entity.Rayon;
-
-public interface RayonRepository extends CrudRepository<Rayon,Long> {
-}
