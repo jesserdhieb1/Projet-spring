f674528f324013c820e88c1e09c4bb3661696fc6
HibaEF <hiba.elwafi@esprit.tn>
2021-10-30 19:16:04 +0100

    first push

---
+++ src/main/java/tn/esprit/spring/repository/FournisseurRepository.java
@@ -0,0 +1,12 @@
+package tn.esprit.spring.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entity.*;
+@Repository
+public interface FournisseurRepository  extends  CrudRepository<Fournisseur, Long> {
+
+
+}
+//aaaaaaaaaaaaaaaaaaaaaaaa
