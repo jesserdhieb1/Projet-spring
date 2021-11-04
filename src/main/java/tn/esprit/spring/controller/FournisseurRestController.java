7666e00b44cb4ea57f3b5d48f79dc582341c4560
HibaEF <hiba.elwafi@esprit.tn>
2021-11-04 21:33:49 +0100

    delete

--- src/main/java/tn/esprit/spring/controller/FournisseurRestController.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/fournisseur")
-public class FournisseurRestController {
-
-}
