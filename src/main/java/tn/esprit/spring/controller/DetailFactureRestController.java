cb43a77719d9fe161fe1f07be1b0993226f1f694
AdamElamri <pearlblack257@gmail.com>
2021-11-05 14:32:29 +0100

    my controllers
    Panier
    Facture
    Detail facture

---
+++ src/main/java/tn/esprit/spring/controller/DetailFactureRestController.java
@@ -0,0 +1,11 @@
+package tn.esprit.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/DetailFacture")
+
+public class DetailFactureRestController {
+
+}
