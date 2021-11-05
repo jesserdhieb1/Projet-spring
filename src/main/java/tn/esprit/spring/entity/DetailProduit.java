bb2e5c6f63351e10b7c4dea27b8845087105c073
AdamElamri <pearlblack257@gmail.com>
2021-11-05 13:52:34 +0100

    Update DetailProduit.java

--- src/main/java/tn/esprit/spring/entity/DetailProduit.java
+++ src/main/java/tn/esprit/spring/entity/DetailProduit.java
@@ -3,20 +3,24 @@ package tn.esprit.spring.entity;
 import tn.esprit.spring.enumeration.CategorieProduit;
 
 import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table
+@Setter
+@Getter
+@NoArgsConstructor
+
+@ToString
 public class DetailProduit implements Serializable {
-    public DetailProduit() {
-    }
-
-    public DetailProduit(Date dateDerniereModification, CategorieProduit categorieProduit) {
-        this.dateDerniereModification = dateDerniereModification;
-        this.CategorieProduit = categorieProduit;
-
-    }
+ 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,44 +34,4 @@ public class DetailProduit implements Serializable {
     private Produit produit;
 
 
-    public Long getIdDetailProduit() {
-        return idDetailProduit;
-    }
-
-    public void setIdDetailProduit(Long idDetailProduit) {
-        this.idDetailProduit = idDetailProduit;
-    }
-
-    public Date getDateDerniereModification() {
-        return dateDerniereModification;
-    }
-
-    public void setDateDerniereModification(Date dateDerniereModification) {
-        this.dateDerniereModification = dateDerniereModification;
-    }
-
-    public CategorieProduit getCategorieProduit() {
-        return CategorieProduit;
-    }
-
-    public void setCategorieProduit(CategorieProduit categorieProduit) {
-        CategorieProduit = categorieProduit;
-    }
-
-    public Produit getProduit() {
-        return produit;
-    }
-
-    public void setProduit(Produit produit) {
-        this.produit = produit;
-    }
-
-    @Override
-    public String toString() {
-        return "detailProduit{" +
-                "idDetailProduit=" + idDetailProduit +
-                ", dateDerniereModification=" + dateDerniereModification +
-                ", CategorieProduit=" + CategorieProduit +
-                '}';
-    }
-}
+   }
