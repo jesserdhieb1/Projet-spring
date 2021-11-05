9e99e2d1b2d9f528a76c890b4f9e099368877db7
OumaymaFarhani <73642477+OumaymaFarhani@users.noreply.github.com>
2021-11-05 15:38:28 +0100

    ProdRepo

--- src/main/java/tn/esprit/spring/service/DetailProduitService.java
+++ src/main/java/tn/esprit/spring/service/DetailProduitService.java
@@ -1,5 +1,9 @@
 package tn.esprit.spring.service;
 
+import tn.esprit.spring.entity.DetailProduit;;
+
 public interface DetailProduitService {
+	void addDetailProduit(DetailProduit detailProduit);
+    DetailProduit getDetailProduitById(long id);
 
 }
--- src/main/java/tn/esprit/spring/service/ProduitServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/ProduitServiceImpl.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entity.Produit;
 
 import tn.esprit.spring.repository.ProduitRepository;
 
+
+@Service
 public class ProduitServiceImpl implements ProduitService{
 	
 	
