047fa6d91e2d740e76860f8e5007d0ab808a0d94
jesserdhieb1 <dhieb.jesser@esprit.tn>
2021-11-06 02:30:34 +0100

    adding methods to  user service

--- src/main/java/tn/esprit/spring/service/UserServiceImp.java
+++ src/main/java/tn/esprit/spring/service/UserServiceImp.java
@@ -1,4 +1,43 @@
 package tn.esprit.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import tn.esprit.spring.entity.User;
+import tn.esprit.spring.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class UserServiceImp implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImp(UserRepository userRepository){this.userRepository=userRepository;}
+
+    @Override
+    public List<User> retrivaAllUsers() {
+        return (List<User>)userRepository.findAll();
+    }
+
+    @Override
+    public User addUser(User u) {
+        userRepository.save(u);
+        return u;
+    }
+
+    @Override
+    public void delteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User updateUser(User u) {
+        userRepository.save(u);
+        return u;
+    }
+
+    @Override
+    public Optional<User> retriveUser(Long id) {
+        return userRepository.findById(id);
+    }
 }
