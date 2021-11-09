74248c9389b3cf2c07d320d75ad234426ec64252
AdamElamri <pearlblack257@gmail.com>
2021-11-09 14:06:07 +0100

    controlleur panier

--- src/main/java/tn/esprit/spring/service/PanierServiceImp.java
+++ src/main/java/tn/esprit/spring/service/PanierServiceImp.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entity.Panier;
 import tn.esprit.spring.repository.PanierRepository;
-
+@Service
 public class PanierServiceImp implements PanierService {
 
 	@Autowired
