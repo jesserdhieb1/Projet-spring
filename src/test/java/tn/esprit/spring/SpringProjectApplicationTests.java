af78ce6e8fd7a93b16ab0b73c0da3f56db62f0ef
jesser dhieb <73405867+jesserdhieb1@users.noreply.github.com>
2021-12-09 15:31:08 +0100

    Test First Filter

--- src/test/java/tn/esprit/spring/SpringProjectApplicationTests.java
+++ src/test/java/tn/esprit/spring/SpringProjectApplicationTests.java
@@ -1,13 +1,33 @@
 package tn.esprit.spring;
-
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.spring.entity.User;
+import tn.esprit.spring.enumeration.CategorieUser;
+import tn.esprit.spring.enumeration.Profession;
+import tn.esprit.spring.service.UserService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 class SpringProjectApplicationTests {
 
+    @Autowired
+    UserService userService;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = dateFormat.parse("2015-03-23");
+        User u1 = new User("jesser","dhieb",date,"jessser@gmail.com", "123", CategorieUser.Prenuim, Profession.Etudiant);
+        userService.addUser(u1);
     }
 
 }
