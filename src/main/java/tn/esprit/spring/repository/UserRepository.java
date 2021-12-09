f3e072062b2d80bc981f87ad4e9e96bc7e055304
jesser dhieb <73405867+jesserdhieb1@users.noreply.github.com>
2021-12-09 12:12:14 +0100

    UserDetailsService

--- src/main/java/tn/esprit/spring/repository/UserRepository.java
+++ src/main/java/tn/esprit/spring/repository/UserRepository.java
@@ -17,6 +17,9 @@ public interface UserRepository extends CrudRepository<User,Long> {
     @Query("SELECT U FROM User U WHERE U.email like %?1")
     Optional<User> findByEmail(String email);
 
+    @Query("SELECT U FROM User U WHERE U.email like %?1")
+    User findByEmail2(String email);
+
     @Modifying
     @Query("UPDATE User U SET U.role=?1 WHERE U.idUser=?2")
     User ChangeRole(Role role, Long id);
