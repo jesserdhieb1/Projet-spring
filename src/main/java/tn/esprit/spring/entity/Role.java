b71c97b46ea2481eaaae7a93d9c88aa80b444bf0
jesser dhieb <73405867+jesserdhieb1@users.noreply.github.com>
2021-12-09 11:35:25 +0100

    adding role / roleName

---
+++ src/main/java/tn/esprit/spring/entity/Role.java
@@ -0,0 +1,25 @@
+package tn.esprit.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
